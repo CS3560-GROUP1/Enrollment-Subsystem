db332e6d428f904839f79b771edc3b6cc263e573
Leonardo Davalos <32350741+leothecrz@users.noreply.github.com>
2022-11-22 20:45:02 -0800

    Static getSQLConnection method added, local sql server instructions in method comments

--- EnrollmentSubSystem/src/main/java/cpp/enrollmentsubsystem/EnrollmentSubSystem.java
+++ EnrollmentSubSystem/src/main/java/cpp/enrollmentsubsystem/EnrollmentSubSystem.java
@@ -6,12 +6,19 @@ import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+
+
 
 /**
  *
@@ -90,6 +97,14 @@ public class EnrollmentSubSystem {
     }
     
     public void start(){
+        
+        try{
+            Connection test = getSQLConnection();
+            Statement s = test.createStatement();
+        } catch (SQLException ex){
+            System.err.println(ex.toString());
+        }
+        
         mainFrame.setVisible(true);
     }
     
@@ -102,4 +117,30 @@ public class EnrollmentSubSystem {
             new EnrollmentSubSystem().start();
         });
     }
+    
+    /**
+     * On Local MySQL server run the following:
+     *  CREATE DATABASE EnrollmentDB;
+     *  CREATE USER 'EnrollmentUser'@'localhost' IDENTIFIED BY '';
+     *  GRANT ALL ON EnrollmentDB.* TO 'EnrollmentUser'@'locahost';
+     * 
+     * 
+     * @return 
+     * @throws java.sql.SQLException 
+     */
+    public static Connection getSQLConnection() throws SQLException{
+        
+        Connection con;
+        String url = "jdbc:mysql://localhost:3306/enrollmentdb";
+        String user = "EnrollmentUser";
+        String pass = "";
+        
+        con = DriverManager.getConnection(url, user, pass);
+        if(con != null){
+            System.out.println(con.toString());
+        }
+        
+        return con;
+    }
+    
 }
